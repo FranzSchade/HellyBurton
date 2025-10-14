eac06c3b4c14122679dcb12c861cbb5151e37bfb
fschade <franz.l.schade@hochschule-stralsund.de>
2025-10-15 00:16:17 +0200

    small tweaks, finished quest system

--- scripts/quest_manager.gd
+++ scripts/quest_manager.gd
@@ -18,9 +18,17 @@ func _ready():
 
 func _process(_delta):
 	if active_quest:
+		var quest_party = len(quest_requiry)
+		var done = 0
 		for key in quest_requiry:
-			if Inventory.get_item(key) == quest_requiry[key]:
+			if Inventory.get_item(key) >= quest_requiry[key]:
+				done += 1
 				print("Requirements fulfilled")
+				quest_requiry.erase(key)
+		if done == quest_party:
+			print("quest_done")
+			active_quest = false
+			quest_text.text = "Done!!"
 
 func load_quest_text() -> Dictionary:
 	if FileAccess.file_exists(quest_text_file):
@@ -38,5 +46,5 @@ func set_quest(quest_key):
 	quest_text.text = current_quest["task"]
 	quest_requiry = current_quest["items"]
 	background_quest.visible = true
-	print(current_quest)
+
 	
